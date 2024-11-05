097b826b361c969e8acd6f2480ebcafba5ade20b
Wesley Yuri <wesleyyuridev@gmail.com>
2024-11-05 00:14:43 -0300

    feat: add opening schema package; feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/WesleyYuri/gopportunities/router"
+import (
+	"github.com/WesleyYuri/gopportunities/config"
+	"github.com/WesleyYuri/gopportunities/router"
+)
+
+var (
+  logger *config.Logger
+)
 
 func main () {
+  logger = config.GetLogger("main")
+  // Initialize Configs
+  err := config.Init()
+  if err != nil {
+    logger.Errf("config initialization error: %v", err)
+    return
+  }
+
   // Initialize Router
   router.Initialize()
 }
